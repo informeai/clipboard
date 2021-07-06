618f0dbf6ffb283462a7f3f410f94216203f1001
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-07-06 18:56:36 -0300

    Add database/store.json :tada:

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+// const { path } = require('path')
+const fs = require('fs')
 
 let win
 function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
-    center: true
+    center: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
   })
 
   win.loadFile(__dirname + '/public/index.html')
+
+  win.webContents.openDevTools()
 }
 
 app
   .whenReady()
   .then(createWindow)
+  .then(getStore)
+  .then(saveStore)
   .catch(err => console.log(err))
 
 // Evente Windows closeds
@@ -25,3 +36,25 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
+
+function getStore() {
+  ipcMain.on('get-store', function (event, args) {
+    let data = JSON.parse(
+      fs.readFileSync(`${__dirname}/database/store.json`, 'utf-8')
+    )
+    console.log(data)
+    event.reply('get-store-reply', data)
+  })
+}
+function saveStore() {
+  ipcMain.on('save-store', function (event, args) {
+    let data = fs.readFileSync(`${__dirname}/database/store.json`, 'utf-8')
+    let arrData = JSON.parse(data)
+    arrData.push(args)
+    fs.writeFileSync(
+      `${__dirname}/database/store.json`,
+      JSON.stringify(arrData)
+    )
+    console.log(arrData.length)
+  })
+}
--- src/public/index.js
+++ src/public/index.js
@@ -1,126 +1,138 @@
 window.onload = function () {
+  const { ipcRenderer } = require('electron')
   let arrText = []
   let list = document.getElementById('list')
   let addText = document.getElementById('add-text')
-  addValuesStore(arrText, addText, list)
-  listText(arrText, list, addText)
-}
+  addValuesStore(ipcRenderer, addText, list)
+  listText(ipcRenderer, list, addText)
 
-function addValuesStore(store, addText, list) {
-  let add = document.getElementById('add')
-  let titleInput = document.querySelector('#title input')
-  let titleSvg = document.querySelector('#title svg')
-  let textTextArea = document.querySelector('#text textarea')
-  let textSvg = document.querySelector('#text svg')
-  let confirm = document.getElementById('confirm')
+  function addValuesStore(ipcRenderer, addText, list) {
+    let add = document.getElementById('add')
+    let titleInput = document.querySelector('#title input')
+    let titleSvg = document.querySelector('#title svg')
+    let textTextArea = document.querySelector('#text textarea')
+    let textSvg = document.querySelector('#text svg')
+    let confirm = document.getElementById('confirm')
 
-  // Event of click in Add
-  add.onclick = function (event) {
-    this.classList.toggle('active')
-    list.classList.add('hide')
-    addText.classList.toggle('hide')
-    if (this.classList.contains('active')) {
-      resetClass()
+    // Event of click in Add
+    add.onclick = function (event) {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+      this.classList.toggle('active')
+      list.classList.add('hide')
+      addText.classList.toggle('hide')
+      if (this.classList.contains('active')) {
+        resetClass()
+      }
     }
-  }
-  // Event of inputs
-  titleInput.addEventListener('input', function () {
-    if (titleInput.value.length > 0) {
-      titleSvg.classList.remove('hide')
-    } else {
-      titleSvg.classList.add('hide')
+    // Event of inputs
+    titleInput.addEventListener('input', function () {
+      if (titleInput.value.length > 0) {
+        titleSvg.classList.remove('hide')
+      } else {
+        titleSvg.classList.add('hide')
+      }
+      // Click titleSvg event
+      titleSvg.addEventListener('click', function () {
+        titleInput.value = ''
+        titleSvg.classList.add('hide')
+        confirm.classList.add('disable')
+      })
+      // Verify input length and remove class disable of button confirm
+      changeConfirmClass(titleInput, textTextArea)
+    })
+
+    textTextArea.addEventListener('input', function () {
+      if (textTextArea.value.length > 0) {
+        textSvg.classList.remove('hide')
+      } else {
+        textSvg.classList.add('hide')
+      }
+      // Click textSvg event
+      textSvg.addEventListener('click', function () {
+        textTextArea.value = ''
+        textSvg.classList.add('hide')
+        confirm.classList.add('disable')
+      })
+      // Verify input length and remove class disable of button confirm
+      changeConfirmClass(titleInput, textTextArea)
+    })
+
+    // Event click button confirm
+    confirm.addEventListener('click', function () {
+      if (!confirm.classList.contains('disable')) {
+        saveText()
+        resetClass()
+      }
+    })
+    // Verify inputs contains any value
+    function changeConfirmClass(title, textarea) {
+      if (
+        textarea.value.length > 0 &&
+        title.value.length > 0 &&
+        titleInput.value != ' ' &&
+        textTextArea.value != ' '
+      ) {
+        confirm.classList.remove('disable')
+      } else {
+        confirm.classList.add('disable')
+      }
     }
-    // Click titleSvg event
-    titleSvg.addEventListener('click', function () {
+    // Reset class list
+    function resetClass() {
       titleInput.value = ''
-      titleSvg.classList.add('hide')
-      confirm.classList.add('disable')
-    })
-    // Verify input length and remove class disable of button confirm
-    changeConfirmClass(titleInput, textTextArea)
-  })
-
-  textTextArea.addEventListener('input', function () {
-    if (textTextArea.value.length > 0) {
-      textSvg.classList.remove('hide')
-    } else {
-      textSvg.classList.add('hide')
-    }
-    // Click textSvg event
-    textSvg.addEventListener('click', function () {
       textTextArea.value = ''
+      titleSvg.classList.add('hide')
       textSvg.classList.add('hide')
       confirm.classList.add('disable')
+    }
+    // Save text in storage
+    function saveText() {
+      let obj = {
+        title: titleInput.value.trim(),
+        text: textTextArea.value.trim()
+      }
+      // store.push(obj)
+      ipcRenderer.send('save-store', obj)
+    }
+  }
+
+  function listText(ipcRenderer, list, addText) {
+    let listText = document.getElementById('list-text')
+    let inputText = document.getElementById('input-text')
+    let add = document.getElementById('add')
+    // Event focus in inputText
+    inputText.addEventListener('focus', function (e) {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+      list.classList.remove('hide')
+      add.classList.remove('active')
+      addText.classList.add('hide')
+      addLi(listText)
     })
-    // Verify input length and remove class disable of button confirm
-    changeConfirmClass(titleInput, textTextArea)
-  })
 
-  // Event click button confirm
-  confirm.addEventListener('click', function () {
-    if (!confirm.classList.contains('disable')) {
-      saveText()
-      resetClass()
+    // Add li in ul
+    function addLi(listText) {
+      ipcRenderer.send('get-store')
+      ipcRenderer.on('get-store-reply', function (event, args) {
+        console.log(args)
+        listText.innerHTML = ''
+        // listText.map(function (el) {
+        //   listText.removeChild(el)
+        // })
+        args.map(function (element) {
+          let li = createElement(element)
+          listText.appendChild(li)
+        })
+      })
     }
-  })
-  // Verify inputs contains any value
-  function changeConfirmClass(title, textarea) {
-    if (
-      textarea.value.length > 0 &&
-      title.value.length > 0 &&
-      titleInput.value != ' ' &&
-      textTextArea.value != ' '
-    ) {
-      confirm.classList.remove('disable')
-    } else {
-      confirm.classList.add('disable')
+
+    // Create li
+    function createElement(objText) {
+      let li = document.createElement('li')
+      let span = document.createElement('span')
+      span.innerText = objText.text
+      li.innerText = objText.title
+      li.appendChild(span)
+      return li
     }
-  }
-  // Reset class list
-  function resetClass() {
-    titleInput.value = ''
-    textTextArea.value = ''
-    titleSvg.classList.add('hide')
-    textSvg.classList.add('hide')
-    confirm.classList.add('disable')
-  }
-  // Save text in storage
-  function saveText() {
-    obj = {
-      title: titleInput.value.trim(),
-      text: textTextArea.value.trim()
-    }
-    store.push(obj)
-  }
-}
-
-function listText(store, list, addText) {
-  let listText = document.getElementById('list-text')
-  let inputText = document.getElementById('input-text')
-  let add = document.getElementById('add')
-  // Event focus in inputText
-  inputText.addEventListener('focusin', function () {
-    list.classList.remove('hide')
-    add.classList.remove('active')
-    addText.classList.add('hide')
-    addLi(listText)
-  })
-
-  // Add li in ul
-  function addLi(listText) {
-    store.forEach(function (element) {
-      let li = createElement(element)
-      listText.appendChild(li)
-    })
-  }
-
-  // Create li
-  function createElement(objText) {
-    let li = document.createElement('li')
-    let span = document.createElement('span')
-    span.innerText = objText.text
-    li.innerText = objText.title
-    li.appendChild(span)
-    return li
   }
 }
